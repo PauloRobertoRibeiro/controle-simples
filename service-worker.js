27acfe2d9fe86442dd6002f0ede757051b18d638
PauloRobertoRibeiro <ribeirorobertopaulo@gmail.com>
2026-02-11 22:46:43 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,41 @@
- const CACHE_NAME = 'controle-simples-cache-v1';
+const CACHE_NAME = "controle-simples-v1";
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
-  // Adicione ícones e outros arquivos estáticos se houver
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./favicon.ico"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
